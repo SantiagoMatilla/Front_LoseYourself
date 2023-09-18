22851cce20fce0c99724ca5ac199d007d3a490d3
Santiago <esematillajr@gmail.com>
2023-09-18 20:48:05 +0200

    footer completed, carousel added on home view and some fisex solved on header

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../modules/home/views/HomeView.vue'
+import ProductsView from '../modules/products/views/ProductsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,13 @@ const router = createRouter({
       name: 'HomeView',
       components: {
         default: HomeView
+      }
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      components: {
+        default: ProductsView
       }
     }
   ]
