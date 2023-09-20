2519a84fedf964a5f500e5863b0fdc0824f982f2
Santiago <esematillajr@gmail.com>
2023-09-20 10:56:18 +0200

    create product form created

---
+++ src/modules/products/views/CreateProductView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import FooterComponent from '../../global/components/FooterComponent.vue';
+import NavBar from '../../global/components/Nav-bar.vue';
+
+
+</script>
+
+<template>
+    <NavBar/>
+    <h1>Crear Productos</h1>
+    <form>
+        <div class="mb-3">
+            <label for="input-product-name" class="form-label">Nombre del Producto</label>
+            <input type="text" class="form-control" id="input-product-name">
+        </div>
+        <div class="mb-3">
+            <label for="input-precio" class="form-label">Precio</label>
+            <input type="text" class="form-control" id="input-precio">
+        </div>
+        <div class="input-group mb-1">
+            <label for="input-select-image" class="form-label">Seleccione una imagen</label>
+        </div>
+        <div class="input-group mb-3">
+            <input type="file" class="form-control" id="input-select-image">
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    
+    <FooterComponent/>
+
+</template>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../modules/home/views/HomeView.vue'
 import ProductsView from '../modules/products/views/ProductsView.vue'
+import CreateProductView from '../modules/products/views/CreateProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,13 @@ const router = createRouter({
       name: 'Products',
       components: {
         default: ProductsView
+      }
+    },
+    {
+      path: '/createProduct',
+      name: 'CreateProduct',
+      components: {
+        default: CreateProductView
       }
     }
   ]
